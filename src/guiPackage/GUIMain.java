d917ff5aeb86f8f8331fcc0e3d0c81b118eff1c1
Aidan Crellin <aidancrellin@gmail.com>
2016-09-05 12:27:59 +1000

    Initial GUI commit

---
+++ src/guiPackage/GUIMain.java
@@ -0,0 +1,95 @@
+package guiPackage;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+import java.awt.Insets;
+
+/**
+ * The Main class of the SRS-SRePs application.
+ * Initialises the GUI and starts the application.
+ * @author Aidan Crellin 9734996
+ *
+ */
+public class GUIMain {
+
+	private JFrame frame;
+	private SalesRecordsTable salesRecordsTable;
+	private UserInputPanel userInputPanel;
+	private UserButtonControlPanel userButtonControlPanel;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIMain window = new GUIMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUIMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 1000, 300);
+		frame.setMinimumSize(new Dimension(800, 300));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("PHP-SRePs");
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		salesRecordsTable = new SalesRecordsTable();
+		
+		GridBagConstraints gbc_SalesRecordsTable = new GridBagConstraints();
+		gbc_SalesRecordsTable.anchor = GridBagConstraints.FIRST_LINE_START;
+		gbc_SalesRecordsTable.fill = GridBagConstraints.HORIZONTAL;
+		gbc_SalesRecordsTable.insets = new Insets(0, 0, 5, 0);
+		gbc_SalesRecordsTable.gridwidth = 3;
+		gbc_SalesRecordsTable.gridx = 0;
+		gbc_SalesRecordsTable.gridy = 0;
+		frame.getContentPane().add(salesRecordsTable.getSalesRecordsScrollPane(), gbc_SalesRecordsTable);
+		
+		userInputPanel = new UserInputPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 3;
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 2;
+		frame.getContentPane().add(userInputPanel, gbc_panel);
+		
+		userButtonControlPanel = new UserButtonControlPanel(salesRecordsTable,userInputPanel);
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridwidth = 3;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 3;
+		frame.getContentPane().add(userButtonControlPanel, gbc_panel_1);	
+				
+	}
+}
---
+++ src/guiPackage/UserInputPanel.java
@@ -0,0 +1,205 @@
+package guiPackage;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
+
+/**
+ * A class to construct the user input fields and panel
+ * JSpinners have been used over JTextFields,
+ * to allow for data validation.
+ * @author Aidan Crellin 9734996
+ *
+ */
+public class UserInputPanel extends JPanel{
+
+	private JComboBox transactionIDComboBox;
+	private JComboBox productNameComboBox;
+	private JSpinner productQuantityField;
+	private JSpinner salePriceFIeld;
+	//private JSpinner totalPriceField; 
+	private JSpinner dateField;
+	private JTextField customerNameField;
+	private ArrayList<String> productNames;
+	private ArrayList<String> transactionIDs;
+	private int transactionIDCount;
+		
+	public UserInputPanel(){
+				
+		transactionIDs = new ArrayList<String>();
+		transactionIDs.add("Select ID");
+		transactionIDs.add("New ID");
+		
+		productNames = new ArrayList<String>();
+		productNames.add("Select Product");
+		for(int i = 0; i < 10; i++)
+		{
+			productNames.add("Product " + (i+1));
+		}
+		
+		JPanel transactionIDPanel = new JPanel();
+		
+		this.add(transactionIDPanel);
+		transactionIDPanel.setLayout(new BoxLayout(transactionIDPanel, BoxLayout.Y_AXIS));
+		
+		JLabel transactionIDLabel = new JLabel("Transaction ID");
+		transactionIDLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		transactionIDLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		transactionIDPanel.add(transactionIDLabel);
+		
+		transactionIDComboBox = new JComboBox();
+		transactionIDComboBox.setModel(new DefaultComboBoxModel(transactionIDs.toArray()));
+		transactionIDComboBox.setSelectedIndex(0);
+		transactionIDPanel.add(transactionIDComboBox);
+		
+		JPanel productNamePanel = new JPanel();
+		this.add(productNamePanel);
+		productNamePanel.setLayout(new BoxLayout(productNamePanel, BoxLayout.Y_AXIS));
+		
+		JLabel productNameLabel = new JLabel("Product Name");
+		productNameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		productNamePanel.add(productNameLabel);
+		
+		productNameComboBox = new JComboBox();
+		productNameComboBox.setModel(new DefaultComboBoxModel(productNames.toArray()));
+		productNameComboBox.setSelectedIndex(0);
+		productNamePanel.add(productNameComboBox);
+		
+		JPanel quantityPanel = new JPanel();
+		this.add(quantityPanel);
+		quantityPanel.setLayout(new BoxLayout(quantityPanel, BoxLayout.Y_AXIS));
+		
+		JLabel productQuantityLabel = new JLabel("Quantity");
+		productQuantityLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		quantityPanel.add(productQuantityLabel);
+		
+		productQuantityField = new JSpinner();
+		quantityPanel.add(productQuantityField);
+		productQuantityField.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
+		
+		JPanel salePricePanel = new JPanel();
+		this.add(salePricePanel);
+		salePricePanel.setLayout(new BoxLayout(salePricePanel, BoxLayout.Y_AXIS));
+		
+		JLabel salePriceLabel = new JLabel("Sale Price");
+		salePriceLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		salePricePanel.add(salePriceLabel);
+		
+		salePriceFIeld = new JSpinner();
+		salePriceFIeld.setModel(new SpinnerNumberModel(new Double(0), new Double(0), null, new Double(1)));
+		salePricePanel.add(salePriceFIeld);
+		
+		/*
+		JPanel totalPricePanel = new JPanel();
+		this.add(totalPricePanel);
+		totalPricePanel.setLayout(new BoxLayout(totalPricePanel, BoxLayout.Y_AXIS));
+		
+		JLabel totalPriceLabel = new JLabel("Total Price");
+		totalPriceLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		totalPricePanel.add(totalPriceLabel);
+		
+		totalPriceField = new JSpinner();
+		totalPriceField.setModel(new SpinnerNumberModel(new Double(0), new Double(0), null, new Double(1)));
+		totalPricePanel.add(totalPriceField);
+		*/
+		
+		JPanel datePanel = new JPanel();
+		this.add(datePanel);
+		datePanel.setLayout(new BoxLayout(datePanel, BoxLayout.Y_AXIS));
+		
+		JLabel dateLabel = new JLabel("Date");
+		dateLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		datePanel.add(dateLabel);
+		
+		dateField = new JSpinner();
+		dateField.setModel(new SpinnerDateModel(new Date(1472652000000L), null, null, Calendar.DAY_OF_YEAR));
+		datePanel.add(dateField);
+		
+		JPanel customerNamePanel = new JPanel();
+		this.add(customerNamePanel);
+		customerNamePanel.setLayout(new BoxLayout(customerNamePanel, BoxLayout.Y_AXIS));
+		
+		JLabel customerNameLabel = new JLabel("Customer Name");
+		customerNameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		customerNamePanel.add(customerNameLabel);
+		
+		customerNameField = new JTextField();
+		customerNamePanel.add(customerNameField);
+		customerNameField.setColumns(10);
+	}
+
+	/**
+	 * Get the combo box which stores all of the transaction IDs
+	 * @return JComboBox containing the transaction IDs
+	 */
+	public JComboBox getTransactionIDComboBox() {
+		return transactionIDComboBox;
+	}
+	
+	/**
+	 * Create a new transaction ID, and add it to the list of current transaction IDs
+	 * @return the newly created transaction ID
+	 */
+	public String addNewIDToTransactionIDComboBox() {
+		transactionIDCount++;
+		transactionIDs.add(Integer.toString(transactionIDCount));
+		transactionIDComboBox.setModel(new DefaultComboBoxModel(transactionIDs.toArray()));
+		return Integer.toString(transactionIDCount);
+	}
+
+	/**
+	 * Get the combo box which stores all of the product names
+	 * @return JComboBox containing the product names
+	 */
+	public JComboBox getProductNameComboBox() {
+		return productNameComboBox;
+	}
+
+	/**
+	 * Get the field that contains number of products sold
+	 * @return JSpinner containing the number of products sold
+	 */
+	public JSpinner getProductQuantityField() {
+		return productQuantityField;
+	}
+
+	/**
+	 * Get the field which contains the price per product paid
+	 * @return JSpinner containing the price per product entered
+	 */
+	public JSpinner getSalePriceField() {
+		return salePriceFIeld;
+	}
+
+	/*public JSpinner getTotalPriceField() {
+		return totalPriceField;
+	}*/
+
+	/**
+	 * Get the field containing the transaction date
+	 * @return JSpinner containing the transaction's date
+	 */
+	public JSpinner getDateField() {
+		return dateField;
+	}
+
+	/**
+	 * Get the text field containing the customers name
+	 * @return JTextField containing the customers Name
+	 */
+	public JTextField getCustomerNameField() {
+		return customerNameField;
+	}
+}
